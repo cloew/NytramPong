a2eda10d78b4483a3bd0249d71f1620e26c2daee
Chris Loew <cloew123@gmail.com>
2015-07-29 10:05:25 -0600

    THe ball now starts with a random direction

--- src/pong/behaviors/ball.py
+++ src/pong/behaviors/ball.py
@@ -1,14 +1,25 @@
 from nytram.ext.box2d import Vec2
 from nytram.ext.box2d.movement import InstantVelocity
 
+import random
+
 class Ball:
     """ Represents a ball's behavior """
     
     def start(self):
         """ Start the body """
-        direction = Vec2(0, 1)
+        direction = self.getInitialDirection()
         speed = InstantVelocity(5)
         speed.apply(self.body, direction)
+        
+    def getInitialDirection(self):
+        """ Return the initial direction for the Ball """
+        directions = [.1, .2, .3, .4, .5, .6, .7, .8, .9, 1]
+        posOrNeg = [-1, 1]
+        
+        x = random.choice(directions)*random.choice(posOrNeg)
+        y = random.choice(directions)*random.choice(posOrNeg)
+        return Vec2(x, y)
         
     @property
     def body(self):

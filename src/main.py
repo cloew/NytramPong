3d41aa64861d0dd78fce2befe5b6ece825e5ad2c
Chris Loew <cloew123@gmail.com>
2015-07-29 08:34:27 -0600

    Added basic application setup

--- src/main.py
+++ src/main.py
@@ -1,8 +1,17 @@
+from nytram import Application, Window
+from nytram.entity.camera import Camera, OrthoProjection
+
 import sys
 
 def main(args):
     """ Run the main file """
-    # Add logic to main
+    window = Window(width=400, height=400, title="Pong")
+    app = Application(window=window)
+    
+    projection = OrthoProjection(width=20, height=20)
+    camera = Camera(eye=[0.0, 10.0, 10.0], projection=projection)
+    
+    app.run()
 
 if __name__ == "__main__":
     main(sys.argv[1:])
